2efa8fe48c630993ec1f68a30230b3a795ddcb0e
abermudez-14 <abermudez@stemdo.io>
2024-10-16 09:46:58 +0200

    Añadir código

--- soluciones/Ejercicio6/Codigo/main.tf
+++ soluciones/Ejercicio6/Codigo/main.tf
@@ -1,1 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+module "vnet_abermudez" {
+    source = "git::github.com/abermudez-14/ejercicio6-tf/modules/vnet_abermudez"
+
+
+    owner_tag = var.owner_tag
+    environment_tag = var.environment_tag
+    location = var.location
+    vnet_address_space = var.vnet_address_space
+    vnet_tags = var.vnet_tags
+    vnet_name = var.vnet_name
+    existent_resource_group_name = var.existent_resource_group_name
+}
 
