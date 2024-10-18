6a4e399cece85410e1fdb29f7384cfaf34c204bb
abermudez-14 <abermudez@stemdo.io>
2024-10-18 14:32:25 +0200

    Añadir codigo

--- soluciones/Ejercicio8/Codigo/main.tf
+++ soluciones/Ejercicio8/Codigo/main.tf
@@ -1,1 +1,32 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "rg-abermudez-dvfinlab"
+    storage_account_name  = "staabermudezdvfinlab"
+    container_name        = "contenedorabermudez"
+    key                   = "terraform.tfstate"  
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "vnet_abermudez" {
+    source = "./modules/vnet_abermudez"
+
+    owner_tag = var.owner_tag
+    environment_tag = var.environment_tag
+    location = var.location
+    vnet_address_space = var.vnet_address_space
+    vnet_tags = var.vnet_tags
+    vnet_name = var.vnet_name
+    existent_resource_group_name = var.existent_resource_group_name
+}
+
+resource "azurerm_storage_container" "ctn_abermudez" {
+  name = "contenedorabermudez"
+  storage_account_name = "staabermudezdvfinlab"
+  container_access_type = "private"
+}
+
 
---
+++ soluciones/Ejercicio8/Codigo/modules/vnet_abermudez/variables.tf
@@ -0,0 +1,31 @@
+variable "owner_tag" {
+
+}
+
+
+
+variable "environment_tag" {
+
+}
+
+
+variable "vnet_tags" {
+
+}
+
+
+variable "existent_resource_group_name" {
+
+}
+
+variable "vnet_name" {
+
+}
+
+variable "vnet_address_space" {
+
+}
+
+variable "location" {
+
+}
